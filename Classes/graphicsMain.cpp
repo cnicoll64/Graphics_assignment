f06a89e401c9ca38f0b4c49fafc66257ff0edd9c
Camemes19 <jamiesoncameron19@gmail.com>
2020-05-01 00:11:03 -0700

    Graphics put into functions, will add to Blokus class tomorrow

--- Classes/graphicsMain.cpp
+++ Classes/graphicsMain.cpp
@@ -2,10 +2,13 @@
 #include "menu.h"
 #include "Blokus.h"
 #include "piece.h"
-#include "Scoreboard.h"
+#include "Player.h"
+#include "tile.h"
+#include "board.h"
 
 int main(void)
 {
+
 	//chooseGame() member function will return either a 2, 3, or 4   - thease numbers stand for amount of users playing
 	//after chooseGame the main Game object should be redefined to work with that amount of players, the rest show be computer players
 	//ie if only two players are playing two computers should be generated
@@ -64,20 +67,24 @@ int main(void)
 	}
 
 	//Create Window
-	//sf::RenderWindow window(sf::VideoMode(1366, 768), "Blokus");
-	
+	sf::RenderWindow window(sf::VideoMode(1366, 768), "Blokus");
+	window.setFramerateLimit(120);
+
+	//Create Font
+	sf::Font gameFont;
+	gameFont.loadFromFile("Righteous-Regular.ttf");
 	//Create Title
-
-	/*sf::Text text("", gameFont);
-	text.setString("Player ");
-	text.setCharacterSize(15);
-	sf::Text playerNum("", gameFont);*/
-
-	//playerNum.setString(currentPlayer.getNum());  <- fix me
-	//text.setCharacterSize(15);
-	//sf::Text textEnd("", gameFont);
-	//playerNum.setString("'s turn");
-	//text.setCharacterSize(15);
+	void setPlayerText(Player p)
+	{
+		string playerColor = to_string(p.getColor());
+		sf::Text textFront("", gameFont);
+		text.setString(playerColor);
+		text.setCharacterSize(15);
+		sf::Text textEnd("", gameFont);
+		playerNum.setString("Player's turn");
+		text.setCharacterSize(15);
+	}
+	
 
 	//text.setColor(sf::Color::Red);
 
@@ -92,121 +99,117 @@ int main(void)
 	tile.setOutlineThickness(2);
 
 	//Create pieces
-	for (int i = 1; i <= 4; i++)
+	sf::Sprite spriteArray[21];
+
+		sf::Sprite p1(sf::Texture::LoadFromFile("Assets/One"));
+		spriteArr[1] = p1;
+		sf::Sprite p2(sf::Texture::LoadFromFile("Assets/Two"));
+		spriteArr[2] = p2;
+		sf::Sprite p3(sf::Texture::LoadFromFile("Assets/Three"));
+		spriteArr[3] = p3;
+		sf::Sprite p4(sf::Texture::LoadFromFile("Assets/Three_Line"));
+		spriteArr[4] = p4;
+		sf::Sprite p5(sf::Texture::LoadFromFile("Assets/Four_Line"));
+		spriteArr[5] = p5;
+		sf::Sprite p6(sf::Texture::LoadFromFile("Assets/Four_Square"));
+		spriteArr[6] = p6;
+		sf::Sprite p7(sf::Texture::LoadFromFile("Assets/Four_L"));
+		spriteArr[7] = p7;
+		sf::Sprite p8(sf::Texture::LoadFromFile("Assets/Four_T"));
+		spriteArr[8] = p8;
+		sf::Sprite p9(sf::Texture::LoadFromFile("Assets/Four_Z"));
+		spriteArr[9] = p9;
+		sf::Sprite p10(sf::Texture::LoadFromFile("Assets/Five_Straight"));
+		spriteArr[10] = p10;
+		sf::Sprite p11(sf::Texture::LoadFromFile("Assets/Five_L"));
+		spriteArr[11] = p11;
+		sf::Sprite p12(sf::Texture::LoadFromFile("Assets/Five_S"));
+		spriteArr[12] = p12;
+		sf::Sprite p13(sf::Texture::LoadFromFile("Assets/Five_T"));
+		spriteArr[13] = p13;
+		sf::Sprite p14(sf::Texture::LoadFromFile("Assets/Five_T_Peg"));
+		spriteArr[14] = p14;
+		sf::Sprite p15(sf::Texture::LoadFromFile("Assets/Five_Z"));
+		spriteArr[15] = p15;
+		sf::Sprite p16(sf::Texture::LoadFromFile("Assets/Five_Square_peg"));
+		spriteArr[16] = p16;
+		sf::Sprite p17(sf::Texture::LoadFromFile("Assets/Five_Cross_Long"));
+		spriteArr[17] = p17;
+		sf::Sprite p18(sf::Texture::LoadFromFile("Assets/Five_C"));
+		spriteArr[18] = p18;
+		sf::Sprite p19(sf::Texture::LoadFromFile("Assets/Five_Bat"));
+		spriteArr[19] = p19;
+		sf::Sprite p20(sf::Texture::LoadFromFile("Assets/Five_Plus"));
+		spriteArr[20] = p20;
+		sf::Sprite p21(sf::Texture::LoadFromFile("Assets/Five_Corner"));
+		spriteArr[21] = p21;
+
+	//draw and maybe tick?
+	void drawBoard(Board* gameBoard)
 	{
-		Piece* p1 = new Piece(i);
-		p1->setShape(1);
-		Piece* p2 = new Piece(i);
-		p2->setShape(1, 2);
-		Piece* p3 = new Piece(i);
-		p3->setShape(1, 2, 7);
-		Piece* p4 = new Piece(i);
-		p4->setShape(1, 2, 3);
-		Piece* p5 = new Piece(i);
-		p5->setShape(1, 2, 6, 7);
-		Piece* p6 = new Piece(i);
-		p6->setShape(2, 6, 7, 8);
-		Piece* p7 = new Piece(i);
-		p7->setShape(1, 2, 3, 4);
-		Piece* p8 = new Piece(i);
-		p8->setShape(3, 6, 7, 8);
-		Piece* p9 = new Piece(i);
-		p9->setShape(2, 3, 6, 7);
-		Piece* p10 = new Piece(i);
-		p10->setShape(1, 6, 7, 8, 9);
-		Piece* p11 = new Piece(i);
-		p11->setShape(2, 7, 11, 12, 13);
-		Piece* p12 = new Piece(i);
-		p12->setShape(1, 6, 11, 12, 13);
-		Piece* p13 = new Piece(i);
-		p13->setShape(2, 3, 4, 6, 7);
-		Piece* p14 = new Piece(i);
-		p14->setShape(3, 6, 7, 8, 11);
-		Piece* p15 = new Piece(i);
-		p15->setShape(1, 6, 11, 16, 21);
-		Piece* p16 = new Piece(i);
-		p16->setShape(1, 6, 11, 7, 12);
-		Piece* p17 = new Piece(i);
-		p17->setShape(2, 3, 6, 7, 11);
-		Piece* p18 = new Piece(i);
-		p18->setShape(1, 2, 6, 11, 12);
-		Piece* p19 = new Piece(i);
-		p19->setShape(2, 3, 6, 7, 12);
-		Piece* p20 = new Piece(i);
-		p20->setShape(2, 6, 7, 8, 12);
-		Piece* p21 = new Piece(i);
-		p21->setShape(2, 6, 7, 8, 9);
+		for (int i = 0; i < 20; i++)
+		{
+			for (int j = 0; j < 20; j++)
+			{
+				tile.setPosition((i * size) + 20, (j * size) + 20);
+				Tile* temp = gameBoard->getGameTile(i, j);
+				switch (temp->getPlayerColor())
+				{
+				case sf::Color::Red:
+					tile.setFillColor(sf::Color::Red);
+					break;
+				case sf::Color::Blue:
+					tile.setFillColor(sf::Color::Blue);
+					break;
+				case sf::Color::Green:
+					tile.setFillColor(sf::Color::Green);
+					break;
+				case sf::Color::Yellow:
+					tile.setFillColor(sf::Color::Yellow);
+					break;
+				default:
+					tile.setFillColor(sf::Color::White);
+					break;
+				}
+				window.draw(tile);
+			}
+		}
 	}
 
-	////draw and maybe tick?
-	//while (window.isOpen())
-	//{
-	//	window.clear();
-
-	//	for (int i = 0; i < 20; i++)
-	//	{
-	//		for (int j = 0; j < 20; j++)
-	//		{
-	//			tile.setPosition((i * size) + 20, (j * size) + 20);
-	//			switch (board[i][j])
-	//			{
-	//			case 1:
-	//				tile.setFillColor(sf::Color::Red);
-	//				break;
-	//			case 2:
-	//				tile.setFillColor(sf::Color::Blue);
-	//				break;
-	//			case 3:
-	//				tile.setFillColor(sf::Color::Green);
-	//				break;
-	//			case 4:
-	//				tile.setFillColor(sf::Color::Yellow);
-	//				break;
-	//			default:
-	//				tile.setFillColor(sf::Color::White);
-	//				break;
-	//			}
-	//			window.draw(tile);
-	//		}
-	//	}
-		/*
-		//displays each player's turn
-		text.setPosition(420, 890);
-		playerNum.setPosition(430, 890);
-		textEnd.setPosition(432, 890);
-		//display each piece still available to the player at hand
+	void drawPieces(BlokusShape piecesArray[21], Player p, sf::Sprite spriteArr[21])
+	{
 		for (int i = 0; i < 21; i++)
 		{
 			//int tempArray[5][5] = piecesArray.getGeo(); <- fix me
-			if (!(piecesArray[i].getPlayed))
+			if (!(piecesArray[i].placed()))
 			{
 				for (int j = 0; j < 5; j++)
 				{
 					for (int k = 0; k < 5; k++)
 					{
-						tileSmall.setPosition((j * 20) + (20 * i), (k * 20) + 600);
-						switch (tempArray[i][j])
+						spriteArr[i].setPosition((j * 20) + (20 * i), (k * 20) + 600);
+						/*switch (p.getColor())
 						{
-						case 1:
+						case sf::Color::Red:
 							tileSmall.setFillColor(sf::Color::Red);
 							break;
-						case 2:
+						case sf::Color:Blue:
 							tileSmall.setFillColor(sf::Color::Blue);
 							break;
-						case 3:
+						case sf::Color::Green:
 							tileSmall.setFillColor(sf::Color::Green);
 							break;
-						case 4:
+						case sf::Color::Yellow:
 							tileSmall.setFillColor(sf::Color::Yellow);
 							break;
 						default:
 							tileSmall.setFillColor(sf::Color::Black);
 							break;
 						}
-						if (PieceArray[i].getSelected)
+						if (PiecesArray[i].getSelected)
 						{
 							tileSmall.setOutlineThickness(4);
-						}
+						}*/
 						window.draw(tile);
 					}
 				}
@@ -214,17 +217,28 @@ int main(void)
 			}
 			//print each piece for the player(scaled?)
 		}
+	}
+
+	while (window.isOpen())
+	{
+		window.clear();
+
+		drawBoard(Board* gameBoard);
+		drawPieces(BlokusShape piecesArray[21], Player p, spriteArray[]);
+		
+		
+		//displays each player's turn
+		text.setPosition(420, 890);
+		playerNum.setPosition(430, 890);
+		textEnd.setPosition(432, 890);
+		//display each piece still available to the player at hand
+		
 		window.draw(text);
 		window.draw(playerNum);
 		window.draw(textEnd);
 		window.display();
 	}
-
-
-	*/
-
-		//}
-
+	
 
 	return 0;
 }
